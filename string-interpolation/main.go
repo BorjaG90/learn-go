fc1f30012311c254da4cd7380e9288b6aadb02b8
BorjaG90 <borjag90dev@gmail.com>
2022-05-10 14:12:45 +0200

    feat(main.go): experimenting with interpolation #5

--- string-interpolation/main.go
+++ string-interpolation/main.go
@@ -10,15 +10,26 @@ import (
 
 var reader *bufio.Reader
 
+type User struct {
+	UserName        string
+	Age             int
+	FavouriteNumber float64
+	OwnsADog        bool
+}
+
 func main() {
 	reader = bufio.NewReader(os.Stdin)
-	userName := readString("What is your name?")
-	age := readInt("How old are you")
+	var user User
 
-	// fmt.Println("Your name is" + userName + ". You're", age, "years old.")
-	// fmt.Println(fmt.Sprintf("Your name is %s. You're %d years old.", userName, age))
-	// Most efficient
-	fmt.Printf("Your name is %s. You're %d years old.\n", userName, age)
+	user.UserName = readString("What is your name?")
+	user.Age = readInt("How old are you")
+	user.FavouriteNumber = readFloat("What is your favourite number?")
+
+	fmt.Printf("Your name is %s. You're %d years old. Your favourite number is %.4f\n",
+		user.UserName,
+		user.Age,
+		user.FavouriteNumber,
+	)
 
 }
 
@@ -55,6 +66,24 @@ func readInt(s string) int {
 		num, err := strconv.Atoi(userInput)
 		if err != nil {
 			fmt.Println("Please enter a whole number")
+		} else {
+			return num
+		}
+	}
+}
+
+func readFloat(s string) float64 {
+	for {
+		fmt.Println(s)
+		prompt()
+
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		num, err := strconv.ParseFloat(userInput, 64)
+		if err != nil {
+			fmt.Println("Please enter a number")
 		} else {
 			return num
 		}
