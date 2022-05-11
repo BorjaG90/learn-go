6b2bf3f54e840294241a48350d6b0958a7f58bcd
BorjaG90 <borjag90dev@gmail.com>
2022-05-11 20:16:34 +0200

    feat(main.go): slices #7

--- built-in-types-working/main.go
+++ built-in-types-working/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 // reference types (pointers, slices, maps, functions, channels)
 
+// pointers
+/*
 func main() {
 	x := 10
 
@@ -23,4 +28,43 @@ func main() {
 
 func changeValueOfPointer(num *int) {
 	*num = 25
+}
+*/
+
+// slices
+func main() {
+	var animals []string
+	animals = append(animals, "dog")
+	animals = append(animals, "fish")
+	animals = append(animals, "cat")
+	animals = append(animals, "horse")
+
+	fmt.Println(animals)
+
+	//for _, animal := range animals {
+	for i, animal := range animals {
+		fmt.Println(i, animal)
+	}
+
+	fmt.Println("Element 0 is", animals[0])
+	fmt.Println("First two elements are", animals[0:2])
+	fmt.Println("The slice is", len(animals), "elements long")
+
+	// sort
+	fmt.Println("Is it sorted?", sort.StringsAreSorted(animals))
+
+	sort.Strings(animals)
+	fmt.Println("Sorted?", sort.StringsAreSorted(animals))
+	fmt.Println(animals)
+
+	animals = DeleteFromSlice(animals, 1)
+	fmt.Println(animals)
+
+}
+
+func DeleteFromSlice(a []string, i int) []string {
+	a[i] = a[len(a)-1]
+	a[len(a)-1] = ""
+	a = a[:len(a)-1]
+	return a
 }
