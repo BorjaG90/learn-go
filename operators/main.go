8a5a56211cc6e15e8abb70760fa44b33d530aa9b
BorjaG90 <borjag90dev@gmail.com>
2022-05-17 10:19:54 +0200

    feat(*): assignment #13

--- operators/main.go
+++ operators/main.go
@@ -1,44 +1,15 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	a := 12
-	b := 6
+	x := 12
+	x++
 
-	// if b!= 0 {
+	fmt.Println(x)
 
-	// 	c:=divideTwoNumbers(a,b)
+	y := 10
+	y /= 2
+	fmt.Println(y)
 
-	// 	if c==2 {
-	// 		fmt.Println("We found a two")
-	// 	}
-	// }
-
-	// if b != 0 && divideTwoNumbers(a, b) == 2 {
-	// 	fmt.Println("We found a two")
-	// }
-
-	// if divideTwoNumbers(a, b) == 2 && b != 0 {
-	// 	fmt.Println("We found a two")
-	// }
-
-	c, err := divideTwoNumbers(a, b)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		if c == 2 {
-			fmt.Println("We found a two")
-		}
-	}
-}
-
-func divideTwoNumbers(x, y int) (int, error) {
-	if y == 0 {
-		return 0, errors.New("cannot divide by 0")
-	}
-	return x / y, nil
 }
