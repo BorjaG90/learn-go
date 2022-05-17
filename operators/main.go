8424ace74df0f5dca57012feec29ae5ee83b8667
BorjaG90 <borjag90dev@gmail.com>
2022-05-17 10:02:59 +0200

    feat(*): modulus #13

--- operators/main.go
+++ operators/main.go
@@ -3,21 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	// precedence
-	// multiplication
-	a := 12.0 * 3.0 / 4.0
-	b := (12.0 * 3.0) / 4.0
-	c := 12.0 * (3.0 / 4.0)
+	x := 12
+	y := 3
 
-	fmt.Println("a", a, "b", b, "c", c)
+	if x%y == 0 {
+		fmt.Println(y, "divides exactly into", x)
+	} else {
+		fmt.Println(y, "does not divides exactly into", x)
+	}
 
-	// integer division
-	unclear := 12 * (3 / 4)
-	fmt.Println("unclear", unclear)
-
-	// parenthesis
-	f := 12.0 / 3.0 / 4.0
-	fmt.Println("f", f)
-	f = 12.0 / (3.0 / 4.0)
-	fmt.Println("f", f)
+	for m := 1; m <= 12; m++ {
+		fmt.Println("The motnh after", m, "is", m%12)
+	}
 }
